1ec9173c8e42498c6d4490857327a52137b807e9
OmerAtayilmaz <atay.omer33@gmail.com>
2022-09-12 16:36:01 +0300

    db connection created successfully

--- src/main/java/com/trendyol/app/controller/UserController.java
+++ src/main/java/com/trendyol/app/controller/UserController.java
@@ -1,4 +1,17 @@
 package com.trendyol.app.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/v1/")
 public class UserController {
+
+    @GetMapping("/")
+    public ResponseEntity<String> helloWorld(){
+        return ResponseEntity.ok("Hello World");
+    }
+
 }
